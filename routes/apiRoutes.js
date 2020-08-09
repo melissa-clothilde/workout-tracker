9aab19e4d9a6c71a245dc3bb91e35867db965560
Aurora Sky <62398803+melissa-clothilde@users.noreply.github.com>
2020-08-08 17:29:51 -0700

    gets last workout data to appear to page, sans type, name and duration (showing as undefined) properties

---
+++ routes/apiRoutes.js
@@ -0,0 +1,49 @@
+const db = require("../models");
+
+module.exports = (app) => {
+
+  //get workout plan
+  app.get("/api/workouts", (req, res) => {
+    db.Workout.find({})
+      .then(dbWorkout => {
+        res.json(dbWorkout);
+      })
+      .catch(err => {
+        res.status(400).json(err);
+      });
+  });
+
+  //Add exercises to a workout plan
+  app.put("/api/workouts/:id", ({ body }, res) => {
+    db.Workout.create(body)
+      .then(dbWorkout => {
+        res.json(dbWorkout);
+      })
+      .catch(err => {
+        res.status(400).json(err);
+      });
+  });
+
+  //create workout
+  app.post("/api/workouts", ({ body }, res) => {
+    db.Workout.create(body)
+      .then(dbWorkout => {
+        res.json(dbWorkout);
+      })
+      .catch(err => {
+        res.status(400).json(err);
+      });
+  });
+
+  //get workouts range
+  app.get("/api/workouts/range", (req, res) => {
+    db.Workout.find({})
+      .then(dbWorkout => {
+        res.json(dbWorkout);
+      })
+      .catch(err => {
+        res.status(400).json(err);
+      });
+  });
+
+};
--- server.js
+++ server.js
@@ -20,7 +20,7 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
 
 // routes
 require("./routes/htmlRoutes.js")(app);
-// app.use(require("./routes/api.js"));
+require("./routes/apiRoutes.js")(app);
 
 
 app.listen(PORT, () => {
